6665424fe2332b01ea1dbd394786893965fa756d
stephane-klein <contact@stephane-klein.info>
2025-11-24 19:43:38 +0100

    Convert Fastify from Javascript to TypeScript

--- fastify/README.md
+++ fastify/README.md
@@ -1,13 +1,9 @@
 # Fastify API HTTP playground
 
-I haven't started this playground yet.
 ```console
 $ mise install
 $ pnpm install
-$ pnpm run start
-> @ start /home/stephane/git/github.com/stephane-klein/nodejs-api-http-playground/fastify
-> node src/server.js
-
+$ pnpm start
 {"level":30,"time":1764007033876,"pid":570551,"hostname":"t14s","msg":"Server listening at http://[::1]:3000"}
 {"level":30,"time":1764007033877,"pid":570551,"hostname":"t14s","msg":"Server listening at http://127.0.0.1:3000"}
 ```
@@ -18,3 +14,13 @@ In another terminal, execute:
 $ curl http://localhost:3000
 {"hello":"world"}
 ```
+
+## Build application server for production
+
+```console
+$ pnpm run build
+$ pnpm run serve
+{"level":30,"time":1764009397660,"pid":642759,"hostname":"t14s","msg":"Server listening at http://[::1]:3000"}
+{"level":30,"time":1764009397662,"pid":642759,"hostname":"t14s","msg":"Server listening at http://127.0.0.1:3000"}
+```
+
--- fastify/eslint.config.js
+++ fastify/eslint.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "eslint/config";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default defineConfig([
     {
+        files: ['src/*.ts'],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                project: './tsconfig.json'
+            }
+        },
+        plugins: {
+            '@typescript-eslint': typescript
+        },
         rules: {
             semi: "error",
             "array-callback-return": ["error"],
@@ -9,7 +21,14 @@ export default defineConfig([
             "prefer-const": "error",
             "no-duplicate-imports": ["error"],
             "no-irregular-whitespace": "off",
-            "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/consistent-type-imports": "error"
         },
     },
+    {
+        ignores: ['dist/**', 'node_modules/**']
+    }
 ]);
